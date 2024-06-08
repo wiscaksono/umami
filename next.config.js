2cf3cbb645ed6092db4fc98e43dfd66b0615c2c7
Wisnu Wicaksono <wisnu.wicaksono@msiglife.co.id>
2024-06-09 03:57:45 +0700

    allow all headers

--- next.config.js
+++ next.config.js
@@ -30,7 +30,14 @@ const headers = [
     key: 'X-DNS-Prefetch-Control',
     value: 'on',
   },
-  { key: 'Access-Control-Allow-Origin', value: '*' },
+  { key: 'Access-Control-Allow-Credentials', value: 'true' },
+  { key: 'Access-Control-Allow-Origin', value: '*' }, // replace this your actual origin
+  { key: 'Access-Control-Allow-Methods', value: 'GET,DELETE,PATCH,POST,PUT' },
+  {
+    key: 'Access-Control-Allow-Headers',
+    value:
+      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+  },
   {
     key: 'Content-Security-Policy',
     value: contentSecurityPolicy
